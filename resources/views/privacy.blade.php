8442d6126b9132f7f23ff498587325c12cbcc491
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-10-28 19:52:47 -0400

    Create privacy.blade.php

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <div class="sm:max-w-screen-sm px-10">
+        <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="{{ route('home') }}">Go
+            Back</a>
+        <h1 class="text-4xl mt-4 mb-4">Privacy Policy</h1>
+        <p class="mt-2">{{ config('app.name') }} does not store any of your data on the server or database. It only uses
+            your data to export it to a file.</p>
+        <p class="mt-2">The access token is stored in your browser as an encrypted cookie, which is set to expire every
+            2
+            hours (as per YNAB's token expiration policy).</p>
+        <p class="mt-2 font-bold">Your data will <span class="uppercase">not</span> unknowingly be passed to any
+            third-party.</p>
+    </div>
+</x-layout>
