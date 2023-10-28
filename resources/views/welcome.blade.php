f15f8a2e48319dd0d5ae16c9227bf3b9ff461e4d
Daniel Haven <d-haven@outlook.com>
2023-10-28 19:17:16 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,29 +15,37 @@
 </head>
 <body class="antialiased">
 <div
-    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-    <form method="POST" action="{{ route('export') }}">
-        @csrf
+    class="relative flex justify-center items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
 
-        <div>
-            <label>
-                Enter Personal Access Token
-                <input type="text" name="access_token" id="access_token"/>
-            </label>
+    @isset($error)
+        <div class="absolute top-0 left-0 right-0 p-4 bg-red-500 text-white">
+            {{ $error }}
         </div>
-        <div>
-            <label>
-                Select a File Extension
-                <select name="file_extension" id="file_extension">
-                    <option value="csv">CSV</option>
-                    <option value="excel">Excel</option>
-                </select>
-            </label>
-        </div>
-        <div>
-            <button type="submit">Export Transactions</button>
-        </div>
-    </form>
+    @endif
+
+    @if($access_token)
+        <form method="POST" action="{{ route('export') }}" class="space-y-2">
+            @csrf
+
+            <div>
+                <label>
+                    Select a File Extension
+                    <select name="file_extension" id="file_extension">
+                        <option value="csv">CSV</option>
+                        <option value="excel">Excel</option>
+                    </select>
+                </label>
+            </div>
+            <div>
+                <button
+                    class="inline-block px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded active:text-blue-500 hover:bg-blue-700 focus:outline-none focus:ring"
+                    type="submit">Export Transactions
+                </button>
+            </div>
+        </form>
+    @else
+        <a href="{{ $auth_url }}">Authenticate YNAB</a>
+    @endif
 </div>
 </body>
 </html>
