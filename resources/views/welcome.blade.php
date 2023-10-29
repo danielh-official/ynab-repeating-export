ea805a966cb6e6ba6b606d8be80456be3b0264f2
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-10-29 15:14:25 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,16 +9,6 @@
         <form method="POST" action="{{ route('export') }}" class="space-y-4">
             @csrf
 
-            <div class="flex flex-col space-y-2">
-                <label class="relative inline-flex items-center cursor-pointer">
-                    <input name="only_retrieve_changed_records" id="only_retrieve_changed_records" type="checkbox" class="sr-only peer">
-                    <div
-                        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Only Retrieve Changed Records</span>
-                </label>
-                <div class="text-sm text-gray-500 dark:text-gray-400"><p>Only retrieve transactions that have been changed since the last export.</p><p>This option is faster since less data is getting parsed.</p></div>
-            </div>
-
             <div>
                 <label for="file_extension" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Select a File Extension
@@ -38,7 +28,7 @@
             </div>
         </form>
     @else
-        <a href="{{ $auth_url }}">Authenticate YNAB</a>
+        <a class="inline-block px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded active:text-blue-500 hover:bg-blue-700 focus:outline-none focus:ring" href="{{ $auth_url }}">Authenticate YNAB</a>
     @endif
 
     <div class="mt-8 flex flex-col space-y-2 text-center">
