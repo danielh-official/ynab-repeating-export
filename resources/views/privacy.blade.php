4b4dc19792baa44d9683e37cd340306b770ba267
Daniel Haven <d-haven@outlook.com>
2023-10-29 15:36:19 -0400

    Update privacy.blade.php

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -5,7 +5,7 @@
         <h1 class="text-4xl mt-4 mb-4">Privacy Policy</h1>
         <p class="mt-2">{{ config('app.name') }} does not store any of your data on the server or database. It only uses
             your data to export it to a file.</p>
-        <p class="mt-2">The access token is stored in your browser as an encrypted cookie, which is set to expire every
+        <p class="mt-2">The access token is stored on your browser as an encrypted session cookie, which is set to expire every
             2
             hours (as per YNAB's token expiration policy).</p>
         <p class="mt-2 font-bold">Your data will <span class="uppercase">not</span> unknowingly be passed to any
