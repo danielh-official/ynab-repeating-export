1e1f76c9d5321f66638f01cfee369a48032dde5b
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-11-01 20:55:34 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,8 @@
             </div>
         </form>
     @else
-        <a class="inline-block px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded active:text-blue-500 hover:bg-blue-700 focus:outline-none focus:ring" href="{{ $auth_url }}">Authenticate YNAB</a>
+        <a class="inline-block px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded active:text-blue-500 hover:bg-blue-700 focus:outline-none focus:ring"
+           href="{{ $auth_url }}">Authenticate YNAB</a>
     @endif
 
     <div class="mt-8 flex flex-col space-y-2 text-center">
@@ -40,5 +41,30 @@
             <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                href="{{ route('guide') }}">Guide</a>
         </div>
+
+        <hr class="border-gray-300 dark:border-gray-700">
+
+        <form method="POST" action="{{ route('sample.export') }}" class="space-y-2">
+            @csrf
+
+            Want to test the export before connecting to YNAB?
+            <div>
+                <label for="file_extension" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Select a File Extension
+                </label>
+                <select
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    name="file_extension" id="file_extension">
+                    <option value="csv">CSV</option>
+                    <option value="excel">Excel</option>
+                </select>
+            </div>
+            <div>
+                <button
+                    class="inline-block px-12 py-3 text-sm font-medium text-white bg-gray-600 border border-gray-600 rounded active:text-gray-300 hover:text-gray-300 focus:outline-none focus:ring"
+                    type="submit">Export Sample Transactions
+                </button>
+            </div>
+        </form>
     </div>
 </x-layout>
