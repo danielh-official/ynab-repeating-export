a9f7c2b4b1732ad72b6e244c67e8200c088192af
Daniel Haven <d-haven@outlook.com>
2023-11-13 18:15:19 -0500

    Update layout.blade.php
    - Add default title

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name', 'Repeating Export (For YNAB)') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
