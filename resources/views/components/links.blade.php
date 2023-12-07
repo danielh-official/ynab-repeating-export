908f7583b3754289c0195d96483a1ff75546fc13
Daniel Haven <d-haven@outlook.com>
2023-12-07 14:57:37 -0500

    Create links.blade.php

---
+++ resources/views/components/links.blade.php
@@ -0,0 +1,28 @@
+<div class="flex flex-row gap-5 self-center text-sm">
+    <div>
+        <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="{{ route('privacy') }}">Privacy
+            Policy</a>
+    </div>
+    <div>
+        <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+           href="{{ route('guide') }}">Guide</a>
+    </div>
+
+    @if(config('meta.links.github'))
+        <div>
+            <a target="_blank" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+               href="{{ config('meta.links.github') }}">
+                GitHub
+            </a>
+        </div>
+    @endif
+
+    @if(config('meta.links.issues'))
+        <div>
+            <a target="_blank" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+               href="{{ config('meta.links.issues') }}">
+                Have Feedback?
+            </a>
+        </div>
+    @endif
+</div>
