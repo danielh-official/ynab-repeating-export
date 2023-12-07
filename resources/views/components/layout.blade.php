84f72ceb71b422bf98a477847cb0993b2b3e82f3
Daniel Haven <d-haven@outlook.com>
2023-12-07 14:57:28 -0500

    Update layout.blade.php - Use app.js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
 
     <!-- Styles -->
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body class="antialiased">
 <div
