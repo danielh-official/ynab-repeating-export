df772392af2aaa3574d9ceb934cba7ee2c4fa073
Daniel Haven <d-haven@outlook.com>
2023-12-19 18:58:53 -0500

    Update RepeatingTransactionExportTest.php
    - Add new test for tracking whether subtransactions are converted to transactions when exported

--- tests/Feature/Exports/RepeatingTransactionExportTest.php
+++ tests/Feature/Exports/RepeatingTransactionExportTest.php
@@ -81,3 +81,139 @@ test('get collection', function () {
         ->and($result->toArray())->toEqual($expected);
 
 });
+
+test('convert subtransactions into collection', function () {
+    $data = [
+        'id' => 1,
+        'account_id' => 1,
+        'payee_id' => 1,
+        'category_id' => 1,
+        'transfer_account_id' => null,
+        'deleted' => false,
+        'frequency' => 'monthly',
+        'date_first' => '2021-01-01',
+        'date_next' => '2021-01-01',
+        'amount' => -1000,
+        'memo' => 'Parent Memo Test',
+        'flag_color' => 'red',
+        'subtransactions' => [
+            [
+                'id' => 1,
+                'scheduled_transaction_id' => 1,
+                'amount' => -500,
+                'memo' => 'Test',
+                'payee_id' => 1,
+                'category_id' => 1,
+                'transfer_account_id' => null,
+                'deleted' => false,
+            ],
+            [
+                'id' => 2,
+                'scheduled_transaction_id' => 1,
+                'amount' => -500,
+                'memo' => 'Test',
+                'payee_id' => 2,
+                'category_id' => 2,
+                'transfer_account_id' => null,
+                'deleted' => false,
+            ]
+        ],
+    ];
+
+    $categories = collect([
+        [
+            'id' => '1',
+            'name' => 'Test',
+            'category_group_name' => 'Test Group',
+            'deleted' => false,
+        ],
+        [
+            'id' => '2',
+            'name' => 'Test 2',
+            'category_group_name' => 'Test Group 2',
+            'deleted' => false,
+        ]
+    ]);
+
+    $payees = collect([
+        [
+            'id' => '1',
+            'name' => 'Test',
+            'deleted' => false,
+        ],
+        [
+            'id' => '2',
+            'name' => 'Test 2',
+            'deleted' => false,
+        ]
+    ]);
+
+    $accounts = collect([
+        [
+            'id' => '1',
+            'name' => 'Test',
+            'deleted' => false,
+        ],
+    ]);
+
+    $repeatingTransactionExport = resolve(RepeatingTransactionExport::class, [
+        'scheduledTransactions' => collect([$data]),
+        'categories' => $categories,
+        'payees' => $payees,
+        'accounts' => $accounts,
+    ]);
+
+    $result = $repeatingTransactionExport->collection();
+
+    $expected = [
+        [
+            'frequency' => 'monthly',
+            'date_first' => '2021-01-01',
+            'date_next' => '2021-01-01',
+            'amount' => .5,
+            'memo' => 'Test',
+            'flag_color' => 'red',
+            'account_name' => 'Test',
+            'payee_name' => 'Test',
+            'category_name' => 'Test',
+            'category_group_name' => 'Test Group',
+            'transfer_account_name' => null,
+            'raw_amount' => -.5,
+            'inflow_outflow' => 'outflow',
+            'raw_amount_per_week' => -0.13,
+            'raw_amount_per_month' => -0.5,
+            'raw_amount_per_year' => -6.0,
+            'amount_per_week' => 0.13,
+            'amount_per_month' => 0.5,
+            'amount_per_year' => 6.0,
+            'parent_memo' => 'Parent Memo Test',
+            'parent_payee_name' => 'Test',
+        ],
+        [
+            'frequency' => 'monthly',
+            'date_first' => '2021-01-01',
+            'date_next' => '2021-01-01',
+            'amount' => .5,
+            'memo' => 'Test',
+            'flag_color' => 'red',
+            'account_name' => 'Test',
+            'payee_name' => 'Test 2',
+            'category_name' => 'Test 2',
+            'category_group_name' => 'Test Group 2',
+            'transfer_account_name' => null,
+            'raw_amount' => -.5,
+            'inflow_outflow' => 'outflow',
+            'raw_amount_per_week' => -0.13,
+            'raw_amount_per_month' => -0.5,
+            'raw_amount_per_year' => -6.0,
+            'amount_per_week' => 0.13,
+            'amount_per_month' => 0.5,
+            'amount_per_year' => 6.0,
+            'parent_memo' => 'Parent Memo Test',
+            'parent_payee_name' => 'Test',
+        ],
+    ];
+
+    expect($result)->toBeInstanceOf(Collection::class)
+        ->and($result->toArray())->toEqual($expected);
+});
