c26ba428a493e68eddf7e510ced3bb38dc8a95a5
Daniel Haven <d-haven@outlook.com>
2024-05-22 18:57:13 -0400

    refactor: YnabAcceptedFrequencyTest.php
    - organize into dataset

--- tests/Unit/Enums/YnabAcceptedFrequencyTest.php
+++ tests/Unit/Enums/YnabAcceptedFrequencyTest.php
@@ -2,56 +2,18 @@
 
 use App\Enums\YnabAcceptedFrequency;
 
-test('2/month to year equals 24.0', function () {
+test('convert amount from frequency to frequency', function ($amount, $fromFrequency, $toFrequency, $expected) {
     expect(
         YnabAcceptedFrequency::convertAmountFromFrequencyToFrequency(
-            2,
-            YnabAcceptedFrequency::monthly,
-            YnabAcceptedFrequency::yearly)
-    )->toBe(24.0);
-});
-
-test('3/day to year equals 1095.0', function () {
-    expect(
-        YnabAcceptedFrequency::convertAmountFromFrequencyToFrequency(
-            3,
-            YnabAcceptedFrequency::daily,
-            YnabAcceptedFrequency::yearly)
-    )->toBe(1095.0);
-});
-
-test('3/year to month equals .25', function () {
-    expect(
-        YnabAcceptedFrequency::convertAmountFromFrequencyToFrequency(
-            3,
-            YnabAcceptedFrequency::yearly,
-            YnabAcceptedFrequency::monthly)
-    )->toBe(.25);
-});
-
-test('4 per every other week to month equals 8.0', function () {
-    expect(
-        YnabAcceptedFrequency::convertAmountFromFrequencyToFrequency(
-            4,
-            YnabAcceptedFrequency::everyOtherWeek,
-            YnabAcceptedFrequency::monthly)
-    )->toBe(8.0);
-});
-
-test('12 per every 3 months to month equals 4.0', function () {
-    expect(
-        YnabAcceptedFrequency::convertAmountFromFrequencyToFrequency(
-            12,
-            YnabAcceptedFrequency::every3Months,
-            YnabAcceptedFrequency::monthly)
-    )->toBe(4.0);
-});
-
-test('12 per every 4 months to month equals 3.0', function () {
-    expect(
-        YnabAcceptedFrequency::convertAmountFromFrequencyToFrequency(
-            12,
-            YnabAcceptedFrequency::every4Months,
-            YnabAcceptedFrequency::monthly)
-    )->toBe(3.0);
-});
+            $amount,
+            $fromFrequency,
+            $toFrequency)
+    )->toBe($expected);
+})->with([
+    '2/month to year equals 24.0' => [2, YnabAcceptedFrequency::monthly, YnabAcceptedFrequency::yearly, 24.0],
+    '3/day to year equals 1095.0' => [3, YnabAcceptedFrequency::daily, YnabAcceptedFrequency::yearly, 1095.0],
+    '3/year to month equals .25' => [3, YnabAcceptedFrequency::yearly, YnabAcceptedFrequency::monthly, .25],
+    '4 per every other week to month equals 8.0' => [4, YnabAcceptedFrequency::everyOtherWeek, YnabAcceptedFrequency::monthly, 8.0],
+    '12 per every 3 months to month equals 4.0' => [12, YnabAcceptedFrequency::every3Months, YnabAcceptedFrequency::monthly, 4.0],
+    '12 per every 4 months to month equals 3.0' => [12, YnabAcceptedFrequency::every4Months, YnabAcceptedFrequency::monthly, 3.0],
+]);
