96c922447bb6acd12a5b3c9b39e8c2e58ea598aa
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2024-05-22 16:55:53 -0400

    refactor: YnabScheduledTransactionService.php

--- app/Services/YnabScheduledTransactionService.php
+++ app/Services/YnabScheduledTransactionService.php
@@ -11,60 +11,57 @@ class YnabScheduledTransactionService
         private readonly Collection $accounts,
         private readonly Collection $payees,
         private readonly Collection $categories,
-    )
-    {
+    ) {
     }
 
-    /**
-     * @return \Illuminate\Support\Collection
-     */
     public function merge(): \Illuminate\Support\Collection
     {
         $data = collect();
 
         foreach ($this->scheduledTransactions as $scheduledTransaction) {
-            $accountId = data_get($scheduledTransaction, 'account_id');
-            $account = $this->accounts->firstWhere('id', $accountId);
+            $account = $this->accounts->firstWhere('id', data_get($scheduledTransaction, 'account_id'));
 
-            $payeeId = data_get($scheduledTransaction, 'payee_id');
-            $payee = $this->payees->firstWhere('id', $payeeId);
+            $payee = $this->payees->firstWhere('id', data_get($scheduledTransaction, 'payee_id'));
 
-            $categoryId = data_get($scheduledTransaction, 'category_id');
-            $category = $this->categories->firstWhere('id', $categoryId);
+            $category = $this->categories->firstWhere('id', data_get($scheduledTransaction, 'category_id'));
 
-            $transferAccountId = data_get($scheduledTransaction, 'transfer_account_id');
-            $transferAccount = $this->accounts->firstWhere('id', $transferAccountId);
+            $transferAccount = $this->accounts->firstWhere('id',
+                data_get($scheduledTransaction, 'transfer_account_id')
+            );
 
-            $subtransactions = data_get($scheduledTransaction, 'subtransactions');
+            $subtransactions = data_get($scheduledTransaction, 'subtransactions', []);
 
-            if ($subtransactions) {
-                foreach ($subtransactions as $subtransaction) {
-                    $subtransactionCategoryId = data_get($subtransaction, 'category_id');
-                    $subtransactionCategory = $this->categories->firstWhere('id', $subtransactionCategoryId);
+            foreach ($subtransactions as $subtransaction) {
+                $subtransactionCategory = $this->categories->firstWhere('id',
+                    data_get($subtransaction, 'category_id')
+                );
 
-                    $subtransactionTransferAccountId = data_get($subtransaction, 'transfer_account_id');
-                    $subtransactionTransferAccount = $this->accounts->firstWhere('id', $subtransactionTransferAccountId);
+                $subtransactionTransferAccount = $this->accounts->firstWhere('id',
+                    data_get($subtransaction, 'transfer_account_id')
+                );
 
-                    $subtransactionPayeeId = data_get($subtransaction, 'payee_id');
-                    $subtransactionPayee = $this->payees->firstWhere('id', $subtransactionPayeeId);
+                $subtransactionPayee = $this->payees->firstWhere('id',
+                    data_get($subtransaction, 'payee_id')
+                );
 
-                    $data->push([
-                        'deleted' => data_get($scheduledTransaction, 'deleted'),
-                        'frequency' => data_get($scheduledTransaction, 'frequency'),
-                        'date_first' => data_get($scheduledTransaction, 'date_first'),
-                        'date_next' => data_get($scheduledTransaction, 'date_next'),
-                        'amount' => data_get($subtransaction, 'amount'),
-                        'parent_memo' => data_get($scheduledTransaction, 'memo'),
-                        'memo' => data_get($subtransaction, 'memo'),
-                        'flag_color' => data_get($scheduledTransaction, 'flag_color'),
-                        'account' => $account,
-                        'payee' => $subtransactionPayee,
-                        'category' => $subtransactionCategory,
-                        'transfer_account' => $subtransactionTransferAccount,
-                        'parent_payee' => $payee,
-                    ]);
-                }
-            } else {
+                $data->push([
+                    'deleted' => data_get($scheduledTransaction, 'deleted'),
+                    'frequency' => data_get($scheduledTransaction, 'frequency'),
+                    'date_first' => data_get($scheduledTransaction, 'date_first'),
+                    'date_next' => data_get($scheduledTransaction, 'date_next'),
+                    'amount' => data_get($subtransaction, 'amount'),
+                    'parent_memo' => data_get($scheduledTransaction, 'memo'),
+                    'memo' => data_get($subtransaction, 'memo'),
+                    'flag_color' => data_get($scheduledTransaction, 'flag_color'),
+                    'account' => $account,
+                    'payee' => $subtransactionPayee,
+                    'category' => $subtransactionCategory,
+                    'transfer_account' => $subtransactionTransferAccount,
+                    'parent_payee' => $payee,
+                ]);
+            }
+
+            if (! $subtransactions) {
                 $data->push([
                     'deleted' => data_get($scheduledTransaction, 'deleted'),
                     'frequency' => data_get($scheduledTransaction, 'frequency'),
