751b55f3da1ef59eaf6ed27ff7f5f3f068a3561a
Daniel Haven <d-haven@outlook.com>
2024-05-22 16:56:21 -0400

    refactor: YnabScheduledTransactionService.php

--- tests/Unit/Services/YnabScheduledTransactionService.php
+++ tests/Unit/Services/YnabScheduledTransactionService.php
@@ -4,41 +4,37 @@ use App\Services\YnabScheduledTransactionService;
 use Illuminate\Database\Eloquent\Collection;
 
 test('merge empty', function () {
-    $ynabScheduledTransactionService = resolve(YnabScheduledTransactionService::class);
+    $merge = resolve(YnabScheduledTransactionService::class)->merge();
 
-    $data = $ynabScheduledTransactionService->merge();
-
-    expect($data)->toBeInstanceOf(\Illuminate\Support\Collection::class)
-        ->and($data->count())->toBe(0);
+    expect($merge)
+        ->toBeInstanceOf(\Illuminate\Support\Collection::class)
+        ->and($merge->count())->toBe(0);
 });
 
 test('merge scheduled transactions', function () {
-    $scheduledTransactions = new Collection([
-        [
-            'id' => '1',
-            'account_id' => '1',
-            'payee_id' => '1',
-            'category_id' => '1',
-            'transfer_account_id' => null,
-            'deleted' => false,
-            'frequency' => 'monthly',
-            'date_first' => '2021-01-01',
-            'date_next' => '2021-02-01',
-            'amount' => 1000,
-            'memo' => 'memo',
-            'flag_color' => 'red',
-        ],
-    ]);
+    $merge = resolve(YnabScheduledTransactionService::class, [
+        'scheduledTransactions' => new Collection([
+            [
+                'id' => '1',
+                'account_id' => '1',
+                'payee_id' => '1',
+                'category_id' => '1',
+                'transfer_account_id' => null,
+                'deleted' => false,
+                'frequency' => 'monthly',
+                'date_first' => '2021-01-01',
+                'date_next' => '2021-02-01',
+                'amount' => 1000,
+                'memo' => 'memo',
+                'flag_color' => 'red',
+            ],
+        ]),
+    ])->merge();
 
-    $ynabScheduledTransactionService = resolve(YnabScheduledTransactionService::class, [
-        'scheduledTransactions' => $scheduledTransactions,
-    ]);
-
-    $data = $ynabScheduledTransactionService->merge();
-
-    expect($data)->toBeInstanceOf(\Illuminate\Support\Collection::class)
-        ->and($data->count())->toBe(1)
-        ->and($data->first())->toBeArray()->toMatchArray([
+    expect($merge)
+        ->toBeInstanceOf(\Illuminate\Support\Collection::class)
+        ->and($merge->count())->toBe(1)
+        ->and($merge->first())->toBeArray()->toMatchArray([
             'deleted' => false,
             'frequency' => 'monthly',
             'date_first' => '2021-01-01',
@@ -54,41 +50,36 @@ test('merge scheduled transactions', function () {
 });
 
 test('merge scheduled transactions and categories', function () {
-    $scheduledTransactions = new Collection([
-        [
-            'id' => '1',
-            'account_id' => '1',
-            'payee_id' => '1',
-            'category_id' => '1',
-            'transfer_account_id' => null,
-            'deleted' => false,
-            'frequency' => 'monthly',
-            'date_first' => '2021-01-01',
-            'date_next' => '2021-02-01',
-            'amount' => 1000,
-            'memo' => 'memo',
-            'flag_color' => 'red',
-        ],
-    ]);
+    $merge = resolve(YnabScheduledTransactionService::class, [
+        'scheduledTransactions' => new Collection([
+            [
+                'id' => '1',
+                'account_id' => '1',
+                'payee_id' => '1',
+                'category_id' => '1',
+                'transfer_account_id' => null,
+                'deleted' => false,
+                'frequency' => 'monthly',
+                'date_first' => '2021-01-01',
+                'date_next' => '2021-02-01',
+                'amount' => 1000,
+                'memo' => 'memo',
+                'flag_color' => 'red',
+            ],
+        ]),
+        'categories' => new Collection([
+            [
+                'id' => '1',
+                'category_name' => 'category',
+                'category_group_name' => 'category group',
+            ],
+        ]),
+    ])->merge();
 
-    $categories = new Collection([
-        [
-            'id' => '1',
-            'category_name' => 'category',
-            'category_group_name' => 'category group',
-        ],
-    ]);
-
-    $ynabScheduledTransactionService = resolve(YnabScheduledTransactionService::class, [
-        'scheduledTransactions' => $scheduledTransactions,
-        'categories' => $categories,
-    ]);
-
-    $data = $ynabScheduledTransactionService->merge();
-
-    expect($data)->toBeInstanceOf(\Illuminate\Support\Collection::class)
-        ->and($data->count())->toBe(1)
-        ->and($data->first())->toBeArray()->toMatchArray([
+    expect($merge)
+        ->toBeInstanceOf(\Illuminate\Support\Collection::class)
+        ->and($merge->count())->toBe(1)
+        ->and($merge->first())->toBeArray()->toMatchArray([
             'deleted' => false,
             'frequency' => 'monthly',
             'date_first' => '2021-01-01',
@@ -108,40 +99,35 @@ test('merge scheduled transactions and categories', function () {
 });
 
 test('merge scheduled transactions and payees', function () {
-    $scheduledTransactions = new Collection([
-        [
-            'id' => '1',
-            'account_id' => '1',
-            'payee_id' => '1',
-            'category_id' => '1',
-            'transfer_account_id' => null,
-            'deleted' => false,
-            'frequency' => 'monthly',
-            'date_first' => '2021-01-01',
-            'date_next' => '2021-02-01',
-            'amount' => 1000,
-            'memo' => 'memo',
-            'flag_color' => 'red',
-        ],
-    ]);
+    $merge = resolve(YnabScheduledTransactionService::class, [
+        'scheduledTransactions' => new Collection([
+            [
+                'id' => '1',
+                'account_id' => '1',
+                'payee_id' => '1',
+                'category_id' => '1',
+                'transfer_account_id' => null,
+                'deleted' => false,
+                'frequency' => 'monthly',
+                'date_first' => '2021-01-01',
+                'date_next' => '2021-02-01',
+                'amount' => 1000,
+                'memo' => 'memo',
+                'flag_color' => 'red',
+            ],
+        ]),
+        'payees' => new Collection([
+            [
+                'id' => '1',
+                'name' => 'payee',
+            ],
+        ]),
+    ])->merge();
 
-    $payees = new Collection([
-        [
-            'id' => '1',
-            'name' => 'payee',
-        ],
-    ]);
-
-    $ynabScheduledTransactionService = resolve(YnabScheduledTransactionService::class, [
-        'scheduledTransactions' => $scheduledTransactions,
-        'payees' => $payees,
-    ]);
-
-    $data = $ynabScheduledTransactionService->merge();
-
-    expect($data)->toBeInstanceOf(\Illuminate\Support\Collection::class)
-        ->and($data->count())->toBe(1)
-        ->and($data->first())->toBeArray()->toMatchArray([
+    expect($merge)
+        ->toBeInstanceOf(\Illuminate\Support\Collection::class)
+        ->and($merge->count())->toBe(1)
+        ->and($merge->first())->toBeArray()->toMatchArray([
             'deleted' => false,
             'frequency' => 'monthly',
             'date_first' => '2021-01-01',
@@ -160,40 +146,35 @@ test('merge scheduled transactions and payees', function () {
 });
 
 test('merge scheduled transactions and accounts', function () {
-    $scheduledTransactions = new Collection([
-        [
-            'id' => '1',
-            'account_id' => '1',
-            'payee_id' => '1',
-            'category_id' => '1',
-            'transfer_account_id' => null,
-            'deleted' => false,
-            'frequency' => 'monthly',
-            'date_first' => '2021-01-01',
-            'date_next' => '2021-02-01',
-            'amount' => 1000,
-            'memo' => 'memo',
-            'flag_color' => 'red',
-        ],
-    ]);
+    $merge = resolve(YnabScheduledTransactionService::class, [
+        'scheduledTransactions' => new Collection([
+            [
+                'id' => '1',
+                'account_id' => '1',
+                'payee_id' => '1',
+                'category_id' => '1',
+                'transfer_account_id' => null,
+                'deleted' => false,
+                'frequency' => 'monthly',
+                'date_first' => '2021-01-01',
+                'date_next' => '2021-02-01',
+                'amount' => 1000,
+                'memo' => 'memo',
+                'flag_color' => 'red',
+            ],
+        ]),
+        'accounts' => new Collection([
+            [
+                'id' => '1',
+                'name' => 'account',
+            ],
+        ]),
+    ])->merge();
 
-    $accounts = new Collection([
-        [
-            'id' => '1',
-            'name' => 'account',
-        ],
-    ]);
-
-    $ynabScheduledTransactionService = resolve(YnabScheduledTransactionService::class, [
-        'scheduledTransactions' => $scheduledTransactions,
-        'accounts' => $accounts,
-    ]);
-
-    $data = $ynabScheduledTransactionService->merge();
-
-    expect($data)->toBeInstanceOf(\Illuminate\Support\Collection::class)
-        ->and($data->count())->toBe(1)
-        ->and($data->first())->toBeArray()->toMatchArray([
+    expect($merge)
+        ->toBeInstanceOf(\Illuminate\Support\Collection::class)
+        ->and($merge->count())->toBe(1)
+        ->and($merge->first())->toBeArray()->toMatchArray([
             'deleted' => false,
             'frequency' => 'monthly',
             'date_first' => '2021-01-01',
