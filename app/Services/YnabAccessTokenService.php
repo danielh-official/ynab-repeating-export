b4a442269ce1d6621a6ddf5bfb2d42eec08caa32
Daniel Haven <d-haven@outlook.com>
2024-11-13 20:54:49 -0500

    Update YnabAccessTokenService.php

--- app/Services/YnabAccessTokenService.php
+++ app/Services/YnabAccessTokenService.php
@@ -3,31 +3,30 @@
 namespace App\Services;
 
 use Exception;
-use Illuminate\Http\Request;
 
 class YnabAccessTokenService
 {
-    public function store(Request $request, mixed $accessToken): void
+    public function store(mixed $accessToken): void
     {
-        $request->session()->put('ynab_access_token', $accessToken);
+        session()->put('ynab_access_token', $accessToken);
     }
 
     /**
      * @throws Exception
      */
-    public function get(Request $request): mixed
+    public function get(): mixed
     {
-        $accessToken = $request->session()->get('ynab_access_token');
+        $accessToken = session()->get('ynab_access_token');
 
-        if (! $accessToken) {
+        if (empty($accessToken)) {
             throw new Exception('No access token');
         }
 
         return $accessToken;
     }
 
-    public function delete(Request $request): void
+    public function delete(): void
     {
-        $request->session()->forget('ynab_access_token');
+        session()->forget('ynab_access_token');
     }
 }
