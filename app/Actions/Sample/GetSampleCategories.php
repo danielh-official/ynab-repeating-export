3391aa03c01957e646b37b4f3477e63136bd57af
Daniel Haven <daniel.haven@solarreviews.com>
2024-11-13 20:43:32 -0500

    Create GetSampleCategories.php

---
+++ app/Actions/Sample/GetSampleCategories.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Sample;
+
+use App\Models\Category;
+use App\Models\CategoryGroup;
+
+class GetSampleCategories
+{
+    public function handle()
+    {
+        return collect(CategoryGroup::factory()->count(3)->raw([
+            'categories' => Category::factory()->count(3)->raw(),
+        ]));
+    }
+}
