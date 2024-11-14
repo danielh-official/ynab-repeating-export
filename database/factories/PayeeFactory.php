99cc52cfc868170d385e695d93aee753cd2f1b60
Daniel Haven <d-haven@outlook.com>
2024-11-13 20:34:02 -0500

    Create Payee model

---
+++ database/factories/PayeeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payee>
+ */
+class PayeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid,
+            'name' => $this->faker->company,
+            'transfer_account_id' => $this->faker->uuid,
+            'deleted' => false,
+        ];
+    }
+}
