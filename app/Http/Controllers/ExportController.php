7d2da3dd813b7bc84d1c2a232946ad59f5117b0a
Daniel Haven <d-haven@outlook.com>
2024-11-13 20:35:10 -0500

    Update ExportController.php

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -2,69 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\BuildFileName;
+use App\Actions\FlattenCategories;
+use App\Actions\GetAccounts;
+use App\Actions\GetCategories;
+use App\Actions\GetPayees;
+use App\Actions\GetScheduledTransactions;
 use App\Exports\RepeatingTransactionExport;
+use App\Services\YnabAccessTokenService;
 use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
-class ExportController extends BaseExportController
+class ExportController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
+    public function __construct(
+        protected YnabAccessTokenService $ynabAccessTokenService,
+        protected GetScheduledTransactions $getScheduledTransactions,
+        protected GetAccounts $getAccounts,
+        protected GetPayees $getPayees,
+        protected GetCategories $getCategories,
+        protected FlattenCategories $flattenCategories,
+        protected BuildFileName $buildFileName,
+    ) {
+
+    }
+
     public function __invoke(Request $request): Response|BinaryFileResponse|RedirectResponse
     {
         try {
-            $scheduledTransactions = $this->getScheduledTransactions($request);
-        } catch (Exception $e) {
-            if ($e->getCode() === 401) {
-                $this->ynabAccessTokenService->delete($request);
+            $response = $this->getScheduledTransactions->handle($request);
 
-                return redirect()->route('home')->with('error', 'Failed to get scheduled transactions. Please re-authenticate.');
+            $scheduledTransactions = data_get($response->json(), 'data.scheduled_transactions', collect());
+
+            if (!$scheduledTransactions instanceof Collection) {
+                $scheduledTransactions = collect($scheduledTransactions);
             }
-
-            return response($e->getMessage(), 404);
+        } catch (Exception $e) {
+            return $this->handleError($e, $request, 'Failed to get scheduled transactions.');
         }
 
         try {
-            $accounts = $this->getAccounts($request);
-        } catch (Exception $e) {
-            if ($e->getCode() === 401) {
-                $this->ynabAccessTokenService->delete($request);
+            $response = $this->getAccounts->handle($request);
 
-                return redirect()->route('home')->with('error', 'Failed to get accounts. Please re-authenticate.');
+            $accounts = data_get($response->json(), 'data.accounts', collect());
+
+            if (!$accounts instanceof Collection) {
+                $accounts = collect($accounts);
             }
-
-            return response($e->getMessage(), 404);
+        } catch (Exception $e) {
+            return $this->handleError($e, $request, 'Failed to get accounts.');
         }
 
         try {
-            $payees = $this->getPayees($request);
-        } catch (Exception $e) {
-            if ($e->getCode() === 401) {
-                $this->ynabAccessTokenService->delete($request);
+            $response = $this->getPayees->handle($request);
 
-                return redirect()->route('home')->with('error', 'Failed to get payees. Please re-authenticate.');
+            $payees = data_get($response->json(), 'data.payees', collect());
+
+            if (!$payees instanceof Collection) {
+                $payees = collect($payees);
             }
-
-            return response($e->getMessage(), 404);
+        } catch (Exception $e) {
+            return $this->handleError($e, $request, 'Failed to get payees.');
         }
 
         try {
-            $categories = $this->getCategories($request);
-        } catch (Exception $e) {
-            if ($e->getCode() === 401) {
-                $this->ynabAccessTokenService->delete($request);
+            $response = $this->getCategories->handle($request);
 
-                return redirect()->route('home')->with('error', 'Failed to get categories. Please re-authenticate.');
+            $categories = data_get($response->json(), 'data.category_groups', collect());
+
+            if (!$categories instanceof Collection) {
+                $categories = collect($categories);
             }
 
-            return response($e->getMessage(), 404);
+            $categories = $this->flattenCategories->handle($categories);
+        } catch (Exception $e) {
+            return $this->handleError($e, $request, 'Failed to get categories.');
         }
 
-        $fileName = $this->buildFileName($request);
+        $fileName = $this->buildFileName->handle($request);
 
         return (new RepeatingTransactionExport(
             scheduledTransactions: $scheduledTransactions,
@@ -72,5 +90,18 @@ class ExportController extends BaseExportController
             payees: $payees,
             categories: $categories,
         ))->download($fileName);
+    }
+
+    private function handleError(Exception $e, Request $request, string $customMessage)
+    {
+        if ($e->getCode() === 401) {
+            $this->ynabAccessTokenService->delete($request);
+
+            return redirect()->route('home')->with('error', "$customMessage Please re-authenticate.");
+        }
+
+        $errorMessage = trim(str_replace("\n", ' ', $e->getMessage()));
+
+        return response("$customMessage {$errorMessage}", 503);
     }
 }
