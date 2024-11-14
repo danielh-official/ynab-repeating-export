ec91542e2029323857959e2fc8a0338f8c7a1257
Daniel Haven <d-haven@outlook.com>
2024-11-13 20:33:46 -0500

    Create CategoryGroup model

---
+++ database/factories/CategoryGroupFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CategoryGroup>
+ */
+class CategoryGroupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid,
+            'name' => $this->faker->name,
+            'hidden' => $this->faker->boolean,
+            'deleted' => false,
+        ];
+    }
+}
